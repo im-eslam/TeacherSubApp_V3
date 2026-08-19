9d23f70c11cb209bbab34df34976d4cc49350b5b
asu-eg <abdelrahman1eslam@gmail.com>
2026-08-19 20:15:02 +0300

    review(B.1-Teachers)_V1

--- TeacherSubApp.Api/Common/4.Extensions/ServiceCollectionExtensions.cs
+++ TeacherSubApp.Api/Common/4.Extensions/ServiceCollectionExtensions.cs
@@ -7,7 +7,7 @@ using TeacherSubApp.Api.Data;
 using TeacherSubApp.Api.Features.EventKeys;
 using TeacherSubApp.Api.Features.SchoolClasses;
 using TeacherSubApp.Api.Features.Subjects;
-//using TeacherSubApp.Api.Features.Teachers;
+using TeacherSubApp.Api.Features.Teachers;
 //using TeacherSubApp.Api.Features.WeeklySchedules;
 
 namespace TeacherSubApp.Api.Common.Extensions
@@ -42,7 +42,7 @@ namespace TeacherSubApp.Api.Common.Extensions
             services.AddScoped<ISubjectService, SubjectService>();
             services.AddScoped<ISchoolClassService, SchoolClassService>();
             services.AddScoped<IEventKeyService, EventKeyService>();
-            //services.AddScoped<ITeacherService, TeacherService>();
+            services.AddScoped<ITeacherService, TeacherService>();
             //services.AddScoped<IWeeklyScheduleService, WeeklyScheduleService>();
             //services.AddScoped<ITeacherAbsenceService, TeacherAbsenceService>();
             //services.AddScoped<ISubstitutionService, SubstitutionService>();
